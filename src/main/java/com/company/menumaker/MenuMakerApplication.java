a655e5ed372b8ddc1f6871dd965c5ae60d2704d6
ferhadqedirli <ferhadqedirli38@gmail.com>
2022-06-29 02:16:06 +0400

    swagger ui

--- src/main/java/com/company/menumaker/MenuMakerApplication.java
+++ src/main/java/com/company/menumaker/MenuMakerApplication.java
@@ -1,8 +1,17 @@
 package com.company.menumaker;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "menu-maker Service API",
+                description = "menu-maker crud services",
+                version = "v1"
+        )
+)
 @SpringBootApplication
 public class MenuMakerApplication {
 
