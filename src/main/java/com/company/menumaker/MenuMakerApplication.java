7bf0192116fdc408d3b859c67a701fc64bbff090
Sarkhan Rasullu <srasullu@sixclovers.com>
2022-08-11 22:15:31 +0400

    hello world elave edildi

--- src/main/java/com/company/menumaker/MenuMakerApplication.java
+++ src/main/java/com/company/menumaker/MenuMakerApplication.java
@@ -16,6 +16,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MenuMakerApplication {
 
     public static void main(String[] args) {
+        System.out.println("Hello");
         SpringApplication.run(MenuMakerApplication.class, args);
     }
 
