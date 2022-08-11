b0c1a46427ad196575e77fa5e12c2f9e9f836df3
Sarkhan Rasullu <srasullu@sixclovers.com>
2022-08-11 22:23:27 +0400

    Hello world 2 elave edildi

--- src/main/java/com/company/menumaker/MenuMakerApplication.java
+++ src/main/java/com/company/menumaker/MenuMakerApplication.java
@@ -17,6 +17,7 @@ public class MenuMakerApplication {
 
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Hello 2");
         SpringApplication.run(MenuMakerApplication.class, args);
     }
 
